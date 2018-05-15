0fc054185380e6e20a07ca2878f97dd25db7d848
Jhow <jhowliu@gmail.com>
2018-05-15 10:51:23 +0800

    add docker-compose install

--- docker_install.sh
+++ docker_install.sh
@@ -24,3 +24,10 @@ sudo apt-get update
 
 # install docker-CE
 sudo apt-get install docker-ce
+
+# install docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version
